7c47c50547ce7fcc3edb49648699ef5c6596fbb8
Piero Lujan Pedreschi <pierolujanpedreschi@pieros-mbp.lan>
2024-11-05 12:34:35 -0800

    functionality complete

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
+let counterDisplay = document.querySelector('.counter-display');
+let counterMinus = document.querySelector('.counter-minus');
+let counterPlus = document.querySelector('.counter-plus');
+let counterReset = document.querySelector('.counter-reset')
 
+let count = 0;
+updateDisplay();
+
+counterMinus.addEventListener("click", () => {
+    count--;
+    updateDisplay();
+});
+
+counterPlus.addEventListener("click", () => {
+    count++;
+    updateDisplay();
+});
+
+counterReset.addEventListener("click", () => {
+    count = 0;
+    updateDisplay();
+});
+
+function updateDisplay() {
+    counterDisplay.innerHTML = count;
+}
