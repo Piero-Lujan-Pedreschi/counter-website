721d302605225a35568f7e4196e193166f819e5d
Piero Lujan Pedreschi <pierolujanpedreschi@Pieros-MacBook-Pro.local>
2024-11-07 12:20:11 -0800

    finished

--- index.js
+++ index.js
@@ -1,54 +1,43 @@
-// let counterDisplay = document.querySelector('.counter-display');
-// let counterMinus = document.querySelector('.counter-minus');
-// let counterPlus = document.querySelector('.counter-plus');
-// let counterReset = document.querySelector('.counter-reset')
-
-// let count = 0;
-// updateDisplay();
-
-// counterMinus.addEventListener("click", () => {
-//     count--;
-//     updateDisplay();
-// });
-
-// counterPlus.addEventListener("click", () => {
-//     count++;
-//     updateDisplay();
-// });
-
-// counterReset.addEventListener("click", () => {
-//     count = 0;
-//     updateDisplay();
-// });
-
-// function updateDisplay() {
-//     counterDisplay.innerHTML = count;
-// }
-
-
-// const input = document.getElementById("input");
-// const inputDisplay = document.getElementById('input-display');
-
-// input.addEventListener("keyup", () => {
-//     let inputLength = input.value.length;
-//     inputDisplay.textContent = "Input Length: " + inputLength;
-// });
-
-
 const input = document.querySelector('#string-input');
-const submitButton = document.querySelector('.submit-input')
+const submitButton = document.querySelector('.submit-input');
 const inputList = document.querySelector('.input-list-dynamic');
 
-function appendNewString() {
-    let newString = input.value.trim(); //trims excess spaces for a string
 
-    if (newString !== "") {
+function appendNewItem() {
+    let newString = input.value.trim(); 
+
+    if (newString) {
         let listItem = document.createElement('li');
         listItem.textContent = newString;
         inputList.appendChild(listItem);
+
+        let removeButton = document.createElement('button');
+        removeButton.textContent = "X";
+        removeButton.setAttribute('class', 'remove-button');
+        listItem.appendChild(removeButton);
+
+        console.log(inputList);
+
+        console.log("test1");
+        var removeButtons = document.getElementsByClassName('remove-button');
+        for (var i = 0; i < removeButtons.length; i++) {
+            console.log("test2");
+            removeButtons[i].addEventListener("click", function (event) {
+                let listItem = event.target.parentNode;
+                console.log("about to remove");
+                removeItem(listItem);
+            });
+        }
     } else {
         alert("enter valid string");
     }
+    console.log("input Value");
+    input.value = '';
 }
 
-submitButton.addEventListener("click", appendNewString);
+function removeItem(item) {
+    console.log("item removed");
+    inputList.removeChild(item);
+}
+
+
