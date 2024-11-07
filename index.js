c4b9721d63fa91a0f28dec36d71b4b331d312779
Piero Lujan Pedreschi <pierolujanpedreschi@Pieros-MacBook-Pro.local>
2024-11-07 15:50:38 -0800

    done

--- index.js
+++ index.js
@@ -3,12 +3,19 @@ const submitButton = document.querySelector('.submit-input');
 const inputList = document.querySelector('.input-list-dynamic');
 
 
+input.addEventListener("keypress", function(event) {
+    if (event.key === 'Enter') {
+        appendNewItem();
+    }
+});
+
 function appendNewItem() {
     let newString = input.value.trim(); 
 
     if (newString) {
         let listItem = document.createElement('li');
         listItem.textContent = newString;
+        listItem.setAttribute('class', 'list-item');
         inputList.appendChild(listItem);
 
         let removeButton = document.createElement('button');
@@ -16,27 +23,21 @@ function appendNewItem() {
         removeButton.setAttribute('class', 'remove-button');
         listItem.appendChild(removeButton);
 
-        console.log(inputList);
 
-        console.log("test1");
         var removeButtons = document.getElementsByClassName('remove-button');
         for (var i = 0; i < removeButtons.length; i++) {
-            console.log("test2");
             removeButtons[i].addEventListener("click", function (event) {
                 let listItem = event.target.parentNode;
-                console.log("about to remove");
                 removeItem(listItem);
             });
         }
     } else {
         alert("enter valid string");
     }
-    console.log("input Value");
     input.value = '';
 }
 
 function removeItem(item) {
-    console.log("item removed");
     inputList.removeChild(item);
 }
 
