4c8730aeaeadbeed12b84b776db9c7e6779e7ffa
Piero Lujan Pedreschi <pierolujanpedreschi@Pieros-MacBook-Pro.local>
2024-11-08 10:39:08 -0800

    centered

--- index.js
+++ index.js
@@ -18,27 +18,8 @@ function appendNewItem() {
         listItem.setAttribute('class', 'list-item');
         inputList.appendChild(listItem);
 
-        const removeButton = document.createElement('button');
-        removeButton.textContent = "X";
-        removeButton.setAttribute('class', 'remove-button');
-        listItem.appendChild(removeButton);
-        removeButton.addEventListener("click", (event) => {
-            const listItem = event.target.parentNode;
-            inputList.removeChild(listItem);
-        });
-
-        const editButton = document.createElement('button');
-        editButton.textContent = "Edit";
-        editButton.setAttribute('class', 'edit-button');
-        listItem.appendChild(editButton);
-        editButton.addEventListener('click', (event) => {
-            const listItem = event.target.parentNode;
-            const currentInput = listItem.firstChild.textContent;
-            let newInput = prompt('Edit the item:', currentInput);
-            if (newInput !== null) {
-                listItem.firstChild.textContent = newInput;
-            }
-        });
+        editItem(listItem);
+        removeItem(listItem);
     } else {
         alert("enter valid string");
     }
@@ -46,4 +27,33 @@ function appendNewItem() {
 }
 
 
+function removeItem (item) {
+    const removeButton = document.createElement('button');
+    removeButton.textContent = "X";
+    removeButton.setAttribute('class', 'remove-button');
+    item.appendChild(removeButton);
+    removeButton.addEventListener("click", (event) => {
+        const listItem = event.target.parentNode;
+        inputList.removeChild(listItem);
+    });
+}
 
+function editItem (item) {
+    const editButton = document.createElement('button');
+    editButton.textContent = "Edit";
+    editButton.setAttribute('class', 'edit-button');
+    item.appendChild(editButton);
+    editButton.addEventListener('click', (event) => {
+        const listItem = event.target.parentNode;
+        const currentInput = listItem.firstChild.textContent;
+        input.value = currentInput;
+        if (input.value) {
+            console.log("read new input")
+            submitButton.onclick = function () {
+                console.log("new Input");
+                listItem.firstChild.textContent = input.value.trim();
+                submitButton.onclick = appendNewItem();
+            }
+        }
+    });
+}
